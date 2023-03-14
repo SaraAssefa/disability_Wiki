8a83fc36048ba655d9672a18b0d805b21ed6690d
Sara ALEMAYEHU <assefa.sara12@gmail.com>
2023-03-14 11:47:03 +0100

    update

---
+++ user-password.py
@@ -0,0 +1,4 @@
+# (u'WikidataUpdater', BotPassword(u'WikidataUpdater', u'dfcuif32290bvs78g9f1n3tm7sajd39r'))
+# (u'WikibaseAdmin', BotPassword(u'bot_user_1', u'l5qpc3siem65lbc11ri66cokv2sfsvhs'))
+#(u'WikibaseAdmin', BotPassword(u'bot_user_1', u'rgd9gj5jpaflqemnkh8k0vcl3qmde425'))
+(u'WikibaseAdmin', BotPassword(u'saras_bot', u'fbdq9stam0isrfs3ebri69b5j8u5rs8m'))
